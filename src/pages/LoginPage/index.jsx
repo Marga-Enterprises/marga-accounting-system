bca54bd56d6d31da7ea5a8d6c897b705392835d4
unknown <pillorajem10@gmail.com>
2025-06-12 01:47:47 +0800

    Testing CI/CD 4

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -68,7 +68,7 @@ const Page = () => {
     <Box sx={styles.root}>
       <Container sx={styles.brandingContainer}>
         <Typography variant="h2" textAlign="center" sx={styles.title}>
-          Marga Accounting System TEST CI CD
+          Marga Accounting System
         </Typography>
         <Typography variant="h5" textAlign="center" sx={styles.subtitle}>
           Please login to continue
