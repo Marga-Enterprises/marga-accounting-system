b472a83d67c97a963d402054e6eff2c56f9a04d9
unknown <pillorajem10@gmail.com>
2025-06-25 10:49:32 +0800

    Adjusted layout

--- src/pages/PrintInvoicePage/printTemplate.js
+++ src/pages/PrintInvoicePage/printTemplate.js
@@ -24,7 +24,7 @@ export const generatePrintHTML = (content) => `
           justify-content: space-between;
           position: relative;
           width: 100%;
-          margin-top: 3.26rem;
+          margin-top: 3.32rem;
         }
 
         .left-column {
